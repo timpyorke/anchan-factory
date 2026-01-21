86373e11cd871028ac32529cbd4c56fa2c55afdb
thongphitak <thongphitak.tim@gmail.com>
2026-01-21 21:21:09 +0700

    Initial Commit

---
+++ anchan/anchanApp.swift
@@ -0,0 +1,17 @@
+//
+//  anchanApp.swift
+//  anchan
+//
+//  Created by Thongphitak Sowanna on 21/1/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct anchanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
